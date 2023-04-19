4f60e9d800ad3763e7a91f6b290aee97549442c7
abotouq <118174350+hataab22@users.noreply.github.com>
2023-04-19 05:38:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function generateImage() {
       contexts[i].shadowColor = 'rgba(0, 0, 0, 0.7)'
     }
     contexts[0].font = " 100px 'Playfair Display', serif"
-    contexts[0].fillText(name, canvas.width / 2, canvas.height - 280, 880)
+    contexts[0].fillText(name, canvas.width / 2, canvas.height - 300, 1800)
 
     dataURL = canvas.toDataURL()
     updateImage(dataURL)
